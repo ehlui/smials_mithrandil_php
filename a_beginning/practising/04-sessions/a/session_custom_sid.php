0131216b199c2cdffd6e706931c808909bfcfdc2
eHLui <86680816+ehlui@users.noreply.github.com>
2021-07-24 14:49:29 +0200

    Fix if structure

--- a_beginning/practising/04-sessions/a/session_custom_sid.php
+++ a_beginning/practising/04-sessions/a/session_custom_sid.php
@@ -6,10 +6,11 @@
     session_id(CUSTOM_SID);
 
     $is_session=session_start();
-    if ($is_session)
+    if ($is_session){
         echo "Session created ->" . "session_id=".session_id();
         echo NEW_LINE;
         var_dump(session_get_cookie_params());
+    }
 
     $is_deleted=session_destroy();
 
