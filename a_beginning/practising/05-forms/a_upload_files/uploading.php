530f343e554e10c8299583319bbd40bcd16385c6
eHLui <86680816+ehlui@users.noreply.github.com>
2021-07-25 11:25:31 +0200

    Adding error uploading file messages for debugging and check how it works

--- a_beginning/practising/05-forms/a_upload_files/uploading.php
+++ a_beginning/practising/05-forms/a_upload_files/uploading.php
@@ -1,9 +1,18 @@
 <?php
-    session_start();
-    $file_uploaded_array=$_FILES["image-up"];
-    $is_file_array=isset($file_uploaded_array);
+session_start();
+$file_uploaded_array = $_FILES["image-up"];
+$is_file_array = isset($file_uploaded_array);
 
-    if($is_file_array){
-        include_once 'utils.php';
+if ($is_file_array) {
+    include_once 'utils.php';
+    $err_code = $file_uploaded_array['error'];
+    if ($err_code == 0) {
+        $dir_tmp_files = ini_get('upload_tmp_dir');
+        echo $dir_tmp_files;
         print_key_value_array($file_uploaded_array);
-    }
+    } else
+        get_upload_file_error($err_code);
+
+} else {
+    echo "File" . $_FILES["image-up"];
+}
--- a_beginning/practising/05-forms/a_upload_files/utils.php
+++ a_beginning/practising/05-forms/a_upload_files/utils.php
@@ -10,3 +10,19 @@
                 '</li>';
         echo '</ul>';
     }
+
+
+ function get_upload_file_error($err_code){
+     $php_file_upload_errors = array(
+         0 => 'There is no error, the file uploaded with success. code = [UPLOAD_ERR_OK]',
+         1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini .   code =  [UPLOAD_ERR_INI_SIZE]',
+         2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form. code = [UPLOAD_ERR_FORM_SIZE]',
+         3 => 'The uploaded file was only partially uploaded. code = [UPLOAD_ERR_PARTIAL]',
+         4 => 'No file was uploaded. code = [UPLOAD_ERR_NO_FILE]',
+         6 => 'Missing a temporary folder. code = [UPLOAD_ERR_NO_TMP_DIR]',
+         7 => 'Failed to write file to disk. code = [UPLOAD_ERR_CANT_WRITE]',
+         8 => 'A PHP extension stopped the file upload. code = UPLOAD_ERR_EXTENSION',
+     );
+     echo '<p style="color: darkred">'. $php_file_upload_errors[$err_code].'</p>';
+     # https://www.php.net/manual/en/features.file-upload.errors.php
+ }
