2b1b0e455d93551a6fc270c6ae89d7933ba2a9a3
eHLui <86680816+ehlui@users.noreply.github.com>
2021-07-28 00:55:14 +0200

    Menu working with links 	- Now it's time to group by folders

--- a_beginning/practising/menu.php
+++ a_beginning/practising/menu.php
@@ -5,10 +5,25 @@ include_once 'helpers/menu_helps.php';
 $files = extract_files_path_recursively('./');
 
 # 1. Extract parent dirs into an array
-$parent_dirs_array = extract_parent_dirs($files,true);
+$parent_dirs_array = extract_parent_dirs($files, true);
 
 # 2. If path contains the parent it will be store in the array of the parent
-$sub_dir_files = extract_sub_dir_files($parent_dirs_array,$files,true);
+$sub_dir_files = extract_sub_dir_files($parent_dirs_array, $files, true);
 
 # 3. Generate the menu Parent dir with its files
-make_menu($parent_dirs_array,$sub_dir_files);
+#make_menu($parent_dirs_array,$sub_dir_files);
+
+echo '<dl>';
+foreach ($parent_dirs_array as $parent_dir) {
+    echo '<dt>' . $parent_dir . ' </dt>';
+    foreach ($files as $file) {
+        $is_found = strpos($file, $parent_dir);
+        if ($is_found != false) {
+            $split_arr = explode('/', $file);
+            $file_name = array_pop($split_arr);
+            $file_path = implode('/',$split_arr);
+            echo '<dd><i>'.$file_path.'</i><br>&emsp; > <a href="'.$file.'">' . $file_name . '<a/></dd>';
+        }
+    }
+}
+echo '</dl>';
