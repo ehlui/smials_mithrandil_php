2403f5c3ef74ce8339ac0679ca7c954d7ed3ac59
eHLui <86680816+ehlui@users.noreply.github.com>
2021-07-27 22:37:53 +0200

    Refactoring menu and cleaning code 	- Adding all functionalities to a function for cleaning menu.php

--- a_beginning/practising/helpers/menu_helps.php
+++ a_beginning/practising/helpers/menu_helps.php
@@ -1,10 +1,12 @@
 <?php
 
-function clean_string($replace,$for,$str){
-    return str_replace($replace,$for,$str);
+function clean_string($replace, $for, $str)
+{
+    return str_replace($replace, $for, $str);
 }
 
-function extract_files_path_recursivly($path){
+function extract_files_path_recursively($path): array
+{
     $recursive_iterator = new RecursiveIteratorIterator(
         new RecursiveDirectoryIterator($path)
     );
@@ -12,10 +14,58 @@ function extract_files_path_recursivly($path){
     foreach ($recursive_iterator as $el) {
         if ($el->isDir())
             continue;
-        $clean_path=clean_string(
-            './','/',$el->getPathname()
+        $clean_path = clean_string(
+            './', '/', $el->getPathname()
         );
-        array_push($files,$clean_path);
+        array_push($files, $clean_path);
     }
     return $files;
+}
+
+
+function extract_parent_dirs($files, $is_sorted): array
+{
+    $parent_dirs_array = array();
+    foreach ($files as $f) {
+        $split_arr = explode('/', $f);
+        $first_position_not_empty = 1;
+        $parent_dir = $split_arr[$first_position_not_empty];
+        if (is_dir($parent_dir))
+            array_push($parent_dirs_array, $parent_dir);
+    }
+    if ($is_sorted)
+        sort($parent_dirs_array);
+    return array_unique($parent_dirs_array);
+}
+
+
+function extract_sub_dir_files($parent_array, $files_array, $is_sorted): array
+{
+    $sub_dir_files = array();
+    foreach ($files_array as $file) {
+        foreach ($parent_array as $parent_dir) {
+            $is_found = strpos($file, $parent_dir);
+            if ($is_found != false) {
+                array_push($sub_dir_files, $file);
+            }
+        }
+    }
+    if ($is_sorted)
+        sort($sub_dir_files);
+    return array_unique($sub_dir_files);
+}
+
+
+function make_menu($parent_dir_array, $sub_files_array): void
+{
+    echo '<dl>';
+    foreach ($parent_dir_array as $parent_dir) {
+        echo '<dt>' . $parent_dir . ' </dt>';
+        foreach ($sub_files_array as $file) {
+            $is_found = strpos($file, $parent_dir);
+            if ($is_found != false)
+                echo '<dd>' . $file . '</dd>';
+        }
+    }
+    echo '</dl>';
 }
--- a_beginning/practising/menu.php
+++ a_beginning/practising/menu.php
@@ -1,44 +1,14 @@
 <?php
-#session_start();
 include_once 'helpers/menu_helps.php';
 
 # 0. Extract all the files
-$files = extract_files_path_recursivly('./');  #Revisar como que me da 7 repetidos... me esta dando dirs repetidos
+$files = extract_files_path_recursively('./');
 
 # 1. Extract parent dirs into an array
-$parent_dirs_array = array();
-foreach ($files as $f) {
-    $split_arr = explode('/', $f);
-    $first_position_not_empty = 1;
-    $parent_dir = $split_arr[$first_position_not_empty];
-    if (is_dir($parent_dir))
-        array_push($parent_dirs_array, $parent_dir);
-}
-# 1.5 Remove repeated parents
-$parent_dirs_array = array_unique($parent_dirs_array);
+$parent_dirs_array = extract_parent_dirs($files,true);
 
 # 2. If path contains the parent it will be store in the array of the parent
-$sub_dir_files = array();
-foreach ($files as $file) {
-    foreach ($parent_dirs_array as $parent_dir) {
-        $is_found = strpos($file, $parent_dir);
-        if ($is_found != false) {
-            array_push($sub_dir_files, $file);
-        }
-    }
-}
-# 2.5 Sort all the arrays for the menu
-sort($parent_dirs_array);
-sort($sub_dir_files);
+$sub_dir_files = extract_sub_dir_files($parent_dirs_array,$files,true);
 
 # 3. Generate the menu Parent dir with its files
-echo '<dl>';
-foreach ($parent_dirs_array as $parent_dir) {
-    echo '<dt>' . $parent_dir . ' </dt>';
-    foreach ($files as $file) {
-        $is_found = strpos($file, $parent_dir);
-        if ($is_found != false)
-            echo '<dd>'.$file.'</dd>';
-    }
-}
-echo '</dl>';
+make_menu($parent_dirs_array,$sub_dir_files);
